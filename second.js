481bc5996856e04415ffc7679f91e1b9583e6084
Mihir Patel <mmcpatel6305@gmail.com>
2025-07-30 20:34:55 +0530

    only two line in second.js

--- second.js
+++ second.js
@@ -1,7 +1,2 @@
 console.log("This is for second commit with modification");
 console.log("This is second modification");
-
-
-console.log("Feature Notification branch");
-
-console.log("this is bad code");
